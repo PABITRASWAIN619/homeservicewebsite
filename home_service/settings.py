154edac26ac6feb7285c7d79d079ef9e1ea7820b
PABITRASWAIN619 <swainpabitra9861@gmail.com>
2026-07-19 11:52:13 +0530

    fix database ssl configuration

--- home_service/settings.py
+++ home_service/settings.py
@@ -195,28 +195,37 @@ ASGI_APPLICATION = (
 # DATABASE (RENDER POSTGRES FIX)
 # ==========================================
 
+# ==========================================
+# DATABASE
+# ==========================================
 
-DATABASES = {
+if os.environ.get("DATABASE_URL"):
 
+    DATABASES = {
 
-    "default": dj_database_url.config(
+        "default": dj_database_url.config(
 
-        default=(
-            f"sqlite:///{BASE_DIR/'db.sqlite3'}"
-        ),
+            conn_max_age=600,
 
+            ssl_require=True
 
-        conn_max_age=600,
+        )
 
+    }
 
-        ssl_require=True
+else:
 
-    )
+    DATABASES = {
 
-}
+        "default": {
 
+            "ENGINE": "django.db.backends.sqlite3",
 
+            "NAME": BASE_DIR / "db.sqlite3",
 
+        }
+
+    }
 
 # ==========================================
 # PASSWORD VALIDATION
